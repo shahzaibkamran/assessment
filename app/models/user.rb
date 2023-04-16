fb689a8cc560f3a11e6af4fdbbdc346ed631f55b
shahzaibkamran <zebkam67@gmail.com>
2023-04-16 08:53:25 +0500

    Added roles and email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
-  devise :database_authenticatable,:rememberable,:recoverable,
+  devise :database_authenticatable,:rememberable,:recoverable,:confirmable,
          :validatable,:registerable,:jwt_authenticatable,
          jwt_revocation_strategy: self 
+  enum role: [:reader,:admin]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user 
+  end
 
   def jwt_payload
     super
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_224354) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_023625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_224354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti", null: false
+    t.integer "role", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true

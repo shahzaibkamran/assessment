118c5aed8a56a3b1ef5279cc924134cc3c1cd66e
shahzaibkamran <zebkam67@gmail.com>
2023-04-23 07:28:59 +0500

    Fixing elasticsearch heroku

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,8 @@
+if Rails.env == "production"
+    Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['ELASTIC_SEARCH_URL']
+    Searchkick.client = Elasticsearch::Client.new(hosts: ENV['ELASTIC_SEARCH_URL'], retry_on_failure: true, transport_options: {request: {timeout: 250}})
+else
+    url = 'http://localhost:9200/'
+    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
+    Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
+end

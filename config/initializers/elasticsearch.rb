ec2d621bf549ea6b0ab6fadcab114c54f2c27144
shahzaibkamran <zebkam67@gmail.com>
2023-04-23 08:40:24 +0500

    Added ES creds to env

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,14 @@
 if Rails.env == "production"
-    Searchkick.client = Elasticsearch::Client.new(hosts: ENV['ELASTIC_SEARCH_URL'], retry_on_failure: true, transport_options: {request: {timeout: 250}})
+    Searchkick.client = Elasticsearch::Client.new(hosts: [
+        {
+          host: ENV['ELASTIC_SEARCH_URL'],
+          scheme: 'https',
+          path: '',
+          port: 443,
+          user: ENV['ELASTIC_SEARCH_USERNAME'],
+          password: ENV['ELASTIC_SEARCH_PASSWORD']
+        }
+      ], retry_on_failure: true, transport_options: {request: {timeout: 250}})
 else
     url = 'http://localhost:9200/'
     Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})

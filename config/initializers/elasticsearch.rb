fa06847dd619fe817406f6c7295dd5857d49db1f
shahzaibkamran <zebkam67@gmail.com>
2023-04-23 10:24:28 +0500

    Removed password & username from ES init

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,7 @@
 if Rails.env == "production"
     Searchkick.client = Elasticsearch::Client.new(hosts: [
         {
-          host: ENV['ELASTIC_SEARCH_URL'],
-          scheme: 'https',
-          user: ENV['ELASTIC_SEARCH_USERNAME'],
-          password: ENV['ELASTIC_SEARCH_PASSWORD']
+          host: ENV['ELASTIC_SEARCH_URL']
         }
       ], retry_on_failure: true, transport_options: {request: {timeout: 250}})
 else

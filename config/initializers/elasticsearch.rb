fbef2bd3d2cc64f3a5bc6a5e85a435ff81d816ce
shahzaibkamran <zebkam67@gmail.com>
2023-04-23 10:27:08 +0500

    Changes hosts in ES init

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,8 @@
 if Rails.env == "production"
-    Searchkick.client = Elasticsearch::Client.new(hosts: [
-        {
-          host: ENV['ELASTIC_SEARCH_URL']
-        }
-      ], retry_on_failure: true, transport_options: {request: {timeout: 250}})
+    # Searchkick.client = Elasticsearch::Client.new url: ENV['ELASTIC_SEARCH_URL']
+    Searchkick.client = Elasticsearch::Client.new(hosts: ENV['ELASTIC_SEARCH_URL'], retry_on_failure: true, transport_options: {request: {timeout: 250}})
 else
     url = 'http://localhost:9200/'
+    # Searchkick.client = Elasticsearch::Client.new url: url
     Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
 end
